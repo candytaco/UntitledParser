6c4766577d1a3e74f322418e58bcbefeb72caed8
candytaco <candytaco@outlook.com>
2025-03-21 17:06:23 -0700

    write one line per tick about porta lifo

---
+++ Coop Demo Parser/DemoPortalHistory.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
+using DemoParser.Parser.Components.Messages.UserMessages;
+
+namespace Coop_Demo_Parser;
+
+/**
+ * Because multiple portal effect messages can happen on the same tick
+ * these classes take these messages and turn information into more of a
+ * timeseries for the state of the portals across the entire thing
+ */
+public class DemoPortalHistory
+{
+	public List<DemoFramePortals> frames;
+
+	public DemoPortalHistory()
+	{
+		frames = new List<DemoFramePortals>();
+	}
+
+	/**
+	 * Add a new message, assumes that portal closed messages will be given before portal opened messages
+	 */
+	public void AddPortalEffectMessage(PortalFxSurface message, int tick)
+	{
+		PropagateState(tick);
+		DemoFramePortals changedFrame;
+		if (tick == frames.Last().Tick)	// edit last frame in-place
+			changedFrame = frames.Last();
+		else
+		{
+			changedFrame = frames.Last().Copy();
+			frames.Add(changedFrame);
+		}
+
+		int player = message.OwnerEnt;
+		int portal = message.PortalNum;
+
+		switch (message.Effect)
+		{
+			case PortalFizzleType.PortalFizzleSuccess:
+				changedFrame.Player(player).Portal(portal).Position = message.Origin;
+				changedFrame.Player(player).Portal(portal).Orientation = message.Angles;
+				break;
+			case PortalFizzleType.PortalFizzleCantFit:
+				break;
+			case PortalFizzleType.PortalFizzleOverlappedLinked:
+				break;
+			case PortalFizzleType.PortalFizzleBadVolume:
+				break;
+			case PortalFizzleType.PortalFizzleBadSurface:
+				break;
+			case PortalFizzleType.PortalFizzleKilled:
+				changedFrame.Player(player).Portal(portal).Position *= float.NaN;
+				changedFrame.Player(player).Portal(portal).Orientation *= float.NaN;
+				break;
+			case PortalFizzleType.PortalFizzleCleanser:
+				changedFrame.Player(player).Portal(portal).Position *= float.NaN;
+				changedFrame.Player(player).Portal(portal).Orientation *= float.NaN;
+				break;
+			case PortalFizzleType.PortalFizzleClose:
+				changedFrame.Player(player).Portal(portal).Position *= float.NaN;
+				changedFrame.Player(player).Portal(portal).Orientation *= float.NaN;
+				break;
+			case PortalFizzleType.PortalFizzleNearBlue:
+				break;
+			case PortalFizzleType.PortalFizzleNearRed:
+				break;
+			case PortalFizzleType.PortalFizzleNone:
+				break;
+			default:
+				throw new ArgumentOutOfRangeException();
+		}
+	}
+
+	/**
+	 * Propagate previous portal info to current state
+	 */
+	public void PropagateState(int tick)
+	{
+		if (frames.Count < 1)
+		{
+			frames.Add(new DemoFramePortals(0));
+		}
+
+		while (frames.Last().Tick < tick - 1)
+		{
+			frames.Add(new DemoFramePortals(frames.Last()));
+		}
+
+	}
+}
+
+public struct DemoFramePortals
+{
+	public PlayerPortalInfo Player1 = new PlayerPortalInfo();
+	public PlayerPortalInfo Player2 = new PlayerPortalInfo();
+
+	public int Tick { get; private set; }
+
+	public DemoFramePortals(int tick)
+	{
+		Tick = tick;
+	}
+
+	public DemoFramePortals(DemoFramePortals last)
+	{
+		this.Tick = last.Tick + 1;
+		this.Player1 = last.Player1;
+		this.Player2 = last.Player2;
+	}
+
+	public DemoFramePortals Copy()
+	{
+		DemoFramePortals outVal = new DemoFramePortals(Tick + 1);
+		outVal.Player1 = this.Player1.Copy();
+		outVal.Player2 = this.Player2.Copy();
+		return outVal;
+	}
+
+	public void Write(StreamWriter writer)
+	{
+		writer.Write(Tick); writer.Write(",");
+		Player1.Write(writer);
+		Player2.Write(writer);
+	}
+
+	public PlayerPortalInfo Player(int player)
+	{
+		if (player < 2)
+			return this.Player1;
+		return this.Player2;
+	}
+}
+
+public class PlayerPortalInfo
+{
+	public PortalInfo Portal1 = new PortalInfo();
+	public PortalInfo Portal2 = new PortalInfo();
+	public PlayerPortalInfo()
+	{
+	}
+
+	public PlayerPortalInfo(PortalInfo portal1, PortalInfo portal2)
+	{
+		Portal1 = portal1;
+		Portal2 = portal2;
+	}
+
+	public PlayerPortalInfo Copy()
+	{
+		return new PlayerPortalInfo(Portal1.Copy(), Portal2.Copy());
+	}
+
+	public void Write(StreamWriter writer)
+	{
+		Portal1.Write(writer);
+		Portal2.Write(writer);
+	}
+
+	public PortalInfo Portal(int portalIndex)
+	{
+		if (portalIndex < 2)
+			return Portal1;
+		return Portal2;
+	}
+}
+
+public class PortalInfo
+{
+	public Vector3 Position = new Vector3(Single.NaN);
+	public Vector3 Orientation = new Vector3(Single.NaN);
+
+	public PortalInfo()
+	{
+	}
+
+	public PortalInfo(Vector3 position, Vector3 orientation)
+	{
+		Position = position;
+		Orientation = orientation;
+	}
+
+	public PortalInfo(PortalInfo other)
+	{
+		this.Position = other.Position;
+		this.Orientation = other.Orientation;
+	}
+
+	public void Write(StreamWriter writer)
+	{
+		writer.Write("{0},{1},{2},", Position.X, Position.Y, Position.Z);
+		writer.Write("{0},{1},{2},", Orientation.X, Orientation.Y, Orientation.Z);
+	}
+
+	public PortalInfo Copy()
+	{
+		return new PortalInfo(new Vector3(this.Position.X, this.Position.Y, this.Position.Z),
+			new Vector3(this.Orientation.X, this.Orientation.Y, this.Orientation.Z));
+	}
+}
--- Coop Demo Parser/MainWindow.axaml.cs
+++ Coop Demo Parser/MainWindow.axaml.cs
@@ -15,6 +15,7 @@ using DemoParser.Parser.Components.Abstract;
 using DemoParser.Parser.Components.Messages;
 using DemoParser.Parser.Components.Messages.UserMessages;
 using Avalonia;
+using Tmds.DBus.Protocol;
 
 namespace Coop_Demo_Parser
 {
@@ -106,42 +107,70 @@ namespace Coop_Demo_Parser
 				if (message.MessageType == UserMessageType.PortalFX_Surface)
 					portalMessages.Add(((PortalFxSurface)message.UserMessage, tick));
 			}
-			portalMessages.Sort((item1, item2) => item1.Item2.CompareTo(item2.Item2));
-			
+			portalMessages.Sort((item1, item2) =>
+			{
+				int tickCompare = item1.Item2.CompareTo(item2.Item2);
+				if (tickCompare != 0)
+					return tickCompare;
+				// additional logic to sort tick info since on one tick, opening a portal will close a previous portal
+				// and for ease of things we want the portal close message to be sorted first
+				if (item1.Item1.Effect == PortalFizzleType.PortalFizzleSuccess)
+					return 1;
+				if (item2.Item1.Effect == PortalFizzleType.PortalFizzleSuccess)
+					return -1;
+				return 0;
+
+			});
+
+			// create frame-by-frame representation of portal states
+			DemoPortalHistory portalHistory = new DemoPortalHistory();
+			foreach ((PortalFxSurface message, int tick) in portalMessages)
+			{
+				portalHistory.AddPortalEffectMessage(message, tick);
+			}
+			portalHistory.PropagateState(demo.TickCount(true));
+
 			string outFileName = Path.Combine(Path.GetFullPath(Path.GetDirectoryName(item.FullFilePath)), Path.GetFileNameWithoutExtension(item.FullFilePath) + " portals.csv");
 			using (StreamWriter writer = new StreamWriter(outFileName))
 			{
-				writer.WriteLine("tick,Portal Entity,Owner Entity,Team,Portal Num,Effect,Origin X,Origin Y, Origin Z,Orientation X,Orientation Y,Orientation Z");
-				foreach ((PortalFxSurface message, int tick) in portalMessages)
+				// writer.WriteLine("tick,Portal Entity,Owner Entity,Team,Portal Num,Effect,Origin X,Origin Y, Origin Z,Orientation X,Orientation Y,Orientation Z");
+				writer.Write("tick,1-1 Origin X,1-1 Origin Y,1-1 Origin Z,1-1 Orientation X,1-1 Orientation Y,1-1 Orientation Z,1-2 Origin X,1-2 Origin Y,1-2 Origin Z,1-2 Orientation X,1-2 Orientation Y,1-2 Orientation Z,");
+				writer.WriteLine("2-1 Origin X,2-1 Origin Y,2-1 Origin Z,2-1 Orientation X,2-1 Orientation Y,2-1 Orientation Z,2-2 Origin X,2-2 Origin Y,2-2 Origin Z,2-2 Orientation X,2-2 Orientation Y,2-2 Orientation Z");
+				foreach (DemoFramePortals frame in portalHistory.frames)
 				{
-					writer.Write(tick); writer.Write(",");
-					writer.Write(message.PortalEnt); writer.Write(",");
-					writer.Write(message.OwnerEnt); writer.Write(",");
-					writer.Write(message.Team); writer.Write(",");
-					writer.Write(message.PortalNum); writer.Write(",");
-					switch (message.Effect)
-					{
-						case PortalFizzleType.PortalFizzleSuccess:
-							writer.Write("+1");
-							break;
-						case PortalFizzleType.PortalFizzleClose:
-							writer.Write("-1");
-							break;
-						case PortalFizzleType.PortalFizzleKilled:
-							writer.Write("-2");
-							break;
-						case PortalFizzleType.PortalFizzleCleanser:
-							writer.Write("-3");
-							break;
-						default:
-							writer.Write("0");
-							break;
-					}
-					writer.Write(",");
-					writer.Write("{0},{1},{2},", message.Origin.X, message.Origin.Y, message.Origin.Z);
-					writer.Write("{0},{1},{2},", message.Angles.X, message.Angles.Y, message.Angles.Z);
+					frame.Write(writer);
 					writer.WriteLine();
 				}
+				/*		foreach ((PortalFxSurface message, int tick) in portalMessages)
+						{
+							writer.Write(tick); writer.Write(",");
+							writer.Write(message.PortalEnt); writer.Write(",");
+							writer.Write(message.OwnerEnt); writer.Write(",");
+							writer.Write(message.Team); writer.Write(",");
+							writer.Write(message.PortalNum); writer.Write(",");
+							switch (message.Effect)
+							{
+								case PortalFizzleType.PortalFizzleSuccess:
+									writer.Write("+1");
+									break;
+								case PortalFizzleType.PortalFizzleClose:
+									writer.Write("-1");
+									break;
+								case PortalFizzleType.PortalFizzleKilled:
+									writer.Write("-2");
+									break;
+								case PortalFizzleType.PortalFizzleCleanser:
+									writer.Write("-3");
+									break;
+								default:
+									writer.Write("0");
+									break;
+							}
+							writer.Write(",");
+							writer.Write("{0},{1},{2},", message.Origin.X, message.Origin.Y, message.Origin.Z);
+							writer.Write("{0},{1},{2},", message.Angles.X, message.Angles.Y, message.Angles.Z);
+							writer.WriteLine();
+				} */
 			}
 		}
 
@@ -158,5 +187,6 @@ namespace Coop_Demo_Parser
 			SourceDemo thisDemo = ((DemoListItem)filesListBox.SelectedItems[0]).demo;
 			SetDemoInfo(thisDemo);
 		}
+		
 	}
 }
